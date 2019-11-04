9fda4800b5f032b6d4e317ca0d1d53692243d140
bihire <muhireboris@yahoo.fr>
2019-11-04 18:56:46 +0200

    ft-ui add signin and signup page - user can signup page - user can signin page - user can switch from signup and signin pages dynamically [Delivers #169545279] [Delivers #169545144]

---
+++ UI/js/welcome.js
@@ -0,0 +1,37 @@
+const signin = document.getElementById('signin')
+const signup = document.getElementById('signup')
+
+const signi = () => {
+    var s = document.getElementsByClassName('signIn')[0].style;
+    s.opacity = 1;
+    function fade() {
+        (s.opacity -= .1) < 0 ? s.display = "none" : setTimeout(fade, 40)
+    }
+    fade()
+    var d = document.getElementsByClassName('signUp')[0].style;
+    d.display = 'block'
+    d.opacity = 1
+    var b = document.getElementById('signin').style
+    var e = document.getElementById('signup').style
+    b.display = 'none'
+    e.display = 'block'
+}
+const signu = () => {
+    var s = document.getElementsByClassName('signUp')[0].style;
+    s.opacity = 1;
+    function fade() {
+        (s.opacity -= .1) < 0 ? s.display = "none" : setTimeout(fade, 40)
+    }
+    fade()
+    var d = document.getElementsByClassName('signIn')[0].style;
+    d.display = 'block'
+    d.opacity = 1
+    var b = document.getElementById('signup').style
+    var e = document.getElementById('signin').style
+    b.display = 'none'
+    e.display = 'block'
+}
+
+signin.addEventListener('click', signi)
+signup.addEventListener('click', signu)
+
