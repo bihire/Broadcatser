fe696fecbba9cf4edec2afc82dfef07cc2e5e24c
bihire <muhireboris@yahoo.fr>
2019-11-18 22:51:41 +0200

    fix error message [Delivers #169729572]

--- server/api/v1/middlewares/authValidation/signinValidator.js
+++ server/api/v1/middlewares/authValidation/signinValidator.js
@@ -16,23 +16,8 @@ export default (req, res, next) => {
     const { error, value } = joi.validate(user, schema);
 
     if (error) {
-        switch (error.details[0].context.key) {
 
-
-            case "email":
-                res.status(400).send({
-                    status: "error",
-                    error: `you must provide a valid email`
-                });
-                break;
-
-            default:
-                res.status(400).send({
-                    status: "error",
-                    error: `invalid information`
-                });
-                break;
-        }
+        responseMsg.errorMsg(res, 400, error.details[0].message)
     } else {
         req.value = value;
         next();
