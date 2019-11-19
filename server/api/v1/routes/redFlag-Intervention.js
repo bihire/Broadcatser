89fe09f13b6212db00cc88b52422e91e15698492
bihire <muhireboris@yahoo.fr>
2019-11-19 21:42:51 +0200

    ft(server) update a comment - add validation on new comment - check if red-flag/intervention exist - check jwt token [Delivers #169848101]

--- server/api/v1/routes/redFlag-Intervention.js
+++ server/api/v1/routes/redFlag-Intervention.js
@@ -3,10 +3,12 @@ import redFlagController from '../controllers/redFlagController'
 import authanticationJWT from "../middlewares/authJWT"
 import createRedFlagValidator from "../middlewares/redFlagValidation/createRedFlagValidator"
 import locationRedFlagValidator from "../middlewares/redFlagValidation/locationRedFlagValidator"
+import commentRedFlagValidator from "../middlewares/redFlagValidation/commentRedFlagValidator"
 
 
 const router = express.Router();
 router.post("/", authanticationJWT,createRedFlagValidator,redFlagController.create)
 router.patch("/:red_flag_id/location", authanticationJWT, locationRedFlagValidator, redFlagController.updateLocation)
+router.patch("/:red_flag_id/comment", authanticationJWT, commentRedFlagValidator, redFlagController.updateComment)
 
 export default router;
