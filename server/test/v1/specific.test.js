cd1c2bd4e5225fbc40358ae6846ccc58cf172074
bihire <muhireboris@yahoo.fr>
2019-11-20 00:54:08 +0200

    ft(server) delete a red-flag/intervention - user should be able to delete their own red-flags/interventions - get response if success or error if error - add testing [Delivers #169853269]

--- server/test/v1/specific.test.js
+++ server/test/v1/specific.test.js
@@ -13,10 +13,24 @@ describe('Fetch red-flag/intervention', () => {
             supertest('http://localhost:8080/api/v1')
                 .get('/red-flags/1')
                 .set('Accept', 'application/json')
-                .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RfbmFtZSI6ImJpaGlyZSIsImxhc3RfbmFtZSI6ImJvcmlzIiwiZW1haWwiOiJtdWhpcmVib3Jpc0B5YWhvby5mciIsInBob25lX251bWJlciI6IjEyMzQ1Njc4OTAiLCJwYXNzd29yZCI6IiQyYSQxMCRVYS9hRnJOeFdnb3hoN29sRUYxNXh1SGZ2NS96czBQcWdxWTVFWnhZcEs3MmJOeDBWUFRNQyIsImlzX2FkbWluIjp0cnVlLCJpYXQiOjE1NzQxNzg4NjJ9.35BfeXMkhkFWvh6CbQiTMxyUBl5O9V8-zIqrC_ewivU')
+                .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RfbmFtZSI6ImJpaGlyZSIsImxhc3RfbmFtZSI6ImJvcmlzIiwiZW1haWwiOiJtdWhpcmVib3Jpc0B5YWhvby5mciIsInBob25lX251bWJlciI6IjEyMzQ1Njc4OTAiLCJwYXNzd29yZCI6IiQyYSQxMCRGaHNucDd3SWh4ZkZYeFAvVVRBY0ZPQ1gxdDVnUng2N1V5Lk8yTk1xZ2ticndTdjJoOWJpeSIsImlzX2FkbWluIjp0cnVlLCJpYXQiOjE1NzQyMDEyNDd9.MEDVcS6wTh45CTHmcR04sltn6GBrGhf9jAl8uYndTdM')
                 .expect('Content-Type', /json/)
                 .end((err, res) => {
                     res.should.have.status(200);
+                    res.should.be.a('object');
+                    done();
+                });
+        });
+    });
+    describe('Get specific red-flag/intervention', () => {
+        it('User should receive an error get red-flag id format', (done) => {
+            supertest('http://localhost:8080/api/v1')
+                .get('/red-flags/g')
+                .set('Accept', 'application/json')
+                .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RfbmFtZSI6ImJpaGlyZSIsImxhc3RfbmFtZSI6ImJvcmlzIiwiZW1haWwiOiJtdWhpcmVib3Jpc0B5YWhvby5mciIsInBob25lX251bWJlciI6IjEyMzQ1Njc4OTAiLCJwYXNzd29yZCI6IiQyYSQxMCRGaHNucDd3SWh4ZkZYeFAvVVRBY0ZPQ1gxdDVnUng2N1V5Lk8yTk1xZ2ticndTdjJoOWJpeSIsImlzX2FkbWluIjp0cnVlLCJpYXQiOjE1NzQyMDEyNDd9.MEDVcS6wTh45CTHmcR04sltn6GBrGhf9jAl8uYndTdM')
+                .expect('Content-Type', /json/)
+                .end((err, res) => {
+                    res.should.have.status(403);
                     res.should.be.a('object');
                     done();
                 });
@@ -27,7 +41,7 @@ describe('Fetch red-flag/intervention', () => {
             supertest('http://localhost:8080/api/v1')
                 .get('/red-flags')
                 .set('Accept', 'application/json')
-                .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RfbmFtZSI6ImJpaGlyZSIsImxhc3RfbmFtZSI6ImJvcmlzIiwiZW1haWwiOiJtdWhpcmVib3Jpc0B5YWhvby5mciIsInBob25lX251bWJlciI6IjEyMzQ1Njc4OTAiLCJwYXNzd29yZCI6IiQyYSQxMCRVYS9hRnJOeFdnb3hoN29sRUYxNXh1SGZ2NS96czBQcWdxWTVFWnhZcEs3MmJOeDBWUFRNQyIsImlzX2FkbWluIjp0cnVlLCJpYXQiOjE1NzQxNzg4NjJ9.35BfeXMkhkFWvh6CbQiTMxyUBl5O9V8-zIqrC_ewivU')
+                .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RfbmFtZSI6ImJpaGlyZSIsImxhc3RfbmFtZSI6ImJvcmlzIiwiZW1haWwiOiJtdWhpcmVib3Jpc0B5YWhvby5mciIsInBob25lX251bWJlciI6IjEyMzQ1Njc4OTAiLCJwYXNzd29yZCI6IiQyYSQxMCRGaHNucDd3SWh4ZkZYeFAvVVRBY0ZPQ1gxdDVnUng2N1V5Lk8yTk1xZ2ticndTdjJoOWJpeSIsImlzX2FkbWluIjp0cnVlLCJpYXQiOjE1NzQyMDEyNDd9.MEDVcS6wTh45CTHmcR04sltn6GBrGhf9jAl8uYndTdM')
                 .expect('Content-Type', /json/)
                 .end((err, res) => {
                     res.should.have.status(200);
