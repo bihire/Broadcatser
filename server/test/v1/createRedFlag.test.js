27a9ca98fbe4750038bfaaa93689fce42fbaf7a6
bihire <muhireboris@yahoo.fr>
2019-12-02 12:35:20 +0200

    add test

--- server/test/v1/createRedFlag.test.js
+++ server/test/v1/createRedFlag.test.js
@@ -1,36 +1,34 @@
-// import chai from 'chai'
-// import chaiHttp from 'chai-http'
-// import supertest from 'supertest'
-// import app from '../../app.js'
+import chai from 'chai'
+import chaiHttp from 'chai-http'
+import supertest from 'supertest'
 
-// chai.use(chaiHttp);
-// chai.should();
-// chai.expect();
+chai.use(chaiHttp);
+chai.should();
+chai.expect();
 
 
-// describe('Testing', () => {
-//     describe('Get a successfull endpoint message', () => {
-//         it('User should receive a successful message', (done) => {
-//             supertest('http://localhost:8080/api/v1/')
-//                 .post('/red-flags')
-//                 .set('Content-type', 'multipart/form-data')
-//                 .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RfbmFtZSI6ImJpaGlyZSIsImxhc3RfbmFtZSI6ImJvcmlzIiwiZW1haWwiOiJtdWhpcmVib3JpQHlhaG9vLmZyIiwicGhvbmVfbnVtYmVyIjoiMTIzNDU2Nzg5MCIsInBhc3N3b3JkIjoiJDJhJDEwJHNJNEhReUVIeUtmUUNxV3lwd21rSk9XY2RVaFBSMFlVUGVrbVkwM3prY04xR1ROZk9PellpIiwiaXNfYWRtaW4iOnRydWUsImlhdCI6MTU3NDk1MzM3Nn0.Qlmyo_ZNTdvkQvARFlUcfqZz-EOWK4bjV3gfC8zTxk0')                
-//                 .attach('image', './sampleUploads/7bBYA.png')
-//                 .field('thisBody', JSON.stringify({
-//                     "title": "this is the tittle",
-//                     "labels": "bro, we @   are, here, ",
-//                     "type": "red-flag",
-//                     "comment": "this is the comment about the red-flag",
-//                     "location": "-90,3"
-//                 }))
-            
-//                 .expect('Content-Type', /json/)
-//                 .end((err, res) => {
-//                     res.should.have.status(201);
-//                     res.should.be.a('object');
-//                     done();
-//                 });
-//         });
-//     });
+describe('Red-flag/Intervention', () => {
+    describe('Post a red-flag/intervention', () => {
+        it('User should receive a successful create message', (done) => {
+            supertest('http://localhost:8080/api/v1/')
+                .post('/red-flags')
+                .set('Accept', 'application/json')
+                .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RfbmFtZSI6ImJpaGlyZSIsImxhc3RfbmFtZSI6ImJvcmlzIiwiZW1haWwiOiJtdWhpcmVib3Jpc0B5YWhvby5mciIsInBob25lX251bWJlciI6IjEyMzQ1Njc4OTAiLCJwYXNzd29yZCI6IiQyYSQxMCRMUE5TeEYyeW1aRjg1bXY5dVNXR3NlaC9KcHJ4WmpzM0xub3VxN3NJWjdYWnk1d3dwdkZOUyIsImlzX2FkbWluIjp0cnVlLCJpYXQiOjE1NzQ5NTUwMTB9.-RlFml5z1stctPsfNnICpktDf-QKGOGHQx33lz_SzP4')
+
+                .send({
+                    "title": "this is the tittle",
+                    "labels": "1",
+                    "type": "intervention",
+                    "comment": "this is the comment about the red-flag",
+                    "location": "-90,3"
+                })
+                .expect('Content-Type', /json/)
+                .end((err, res) => {
+                    res.should.have.status(201);
+                    res.should.be.a('object');
+                    done();
+                });
+        });
+    });
    
-// })
+})
