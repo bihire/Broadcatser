6482bb845e65572707d8a75a987337172218dca8
bihire <muhireboris@yahoo.fr>
2019-12-03 15:12:51 +0200

    ft(server) create red-flag - add a red-flag to database - create a redflag table - add testing with mocha [Delivers #170072627]

--- server/test/v2/createRedFlag.test.js
+++ server/test/v2/createRedFlag.test.js
@@ -12,7 +12,7 @@ chai.expect();
 describe('Red-flag/Intervention', () => {
     describe('Post a red-flag/intervention', () => {
         it('User should receive a successful create message', (done) => {
-            supertest('http://localhost:8080/api/v1')
+            supertest('http://localhost:8080/api/v2')
                 .post('/red-flags')
                 .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RfbmFtZSI6ImJpaGlyZSIsImxhc3RfbmFtZSI6ImJvcmlzIiwiZW1haWwiOiJtdWhpcmVib3Jpc0B5YWhvby5mciIsInBob25lX251bWJlciI6IjEyMzQ1Njc4OTAiLCJwYXNzd29yZCI6IiQyYSQxMCROR0hPMWJzNHNhdXAzN0h3S3Mya3MuUVFyYkRMbHNpWDRHcFNOaVBYcTZCSjFUdlJ2cEFjYSIsImlzX2FkbWluIjp0cnVlLCJpYXQiOjE1NzUwMTcwNTN9.eqtNHB03N5-cQRpWa72-MTXYO7AKJq5as5-_JwEyAp0')
 
@@ -34,7 +34,7 @@ describe('Red-flag/Intervention', () => {
     });
     describe('Post a red-flag/intervention', () => {
         it('User should receive an error create message about invalid input', (done) => {
-            supertest('http://localhost:8080/api/v1')
+            supertest('http://localhost:8080/api/v2')
                 .post('/red-flags')
                 .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RfbmFtZSI6ImJpaGlyZSIsImxhc3RfbmFtZSI6ImJvcmlzIiwiZW1haWwiOiJtdWhpcmVib3Jpc0B5YWhvby5mciIsInBob25lX251bWJlciI6IjEyMzQ1Njc4OTAiLCJwYXNzd29yZCI6IiQyYSQxMCROR0hPMWJzNHNhdXAzN0h3S3Mya3MuUVFyYkRMbHNpWDRHcFNOaVBYcTZCSjFUdlJ2cEFjYSIsImlzX2FkbWluIjp0cnVlLCJpYXQiOjE1NzUwMTcwNTN9.eqtNHB03N5-cQRpWa72-MTXYO7AKJq5as5-_JwEyAp0')
 
@@ -56,7 +56,7 @@ describe('Red-flag/Intervention', () => {
     });
     describe('Post a red-flag/intervention', () => {
         it('User should receive an error create message about invalid location input', (done) => {
-            supertest('http://localhost:8080/api/v1')
+            supertest('http://localhost:8080/api/v2')
                 .post('/red-flags')
                 .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RfbmFtZSI6ImJpaGlyZSIsImxhc3RfbmFtZSI6ImJvcmlzIiwiZW1haWwiOiJtdWhpcmVib3Jpc0B5YWhvby5mciIsInBob25lX251bWJlciI6IjEyMzQ1Njc4OTAiLCJwYXNzd29yZCI6IiQyYSQxMCROR0hPMWJzNHNhdXAzN0h3S3Mya3MuUVFyYkRMbHNpWDRHcFNOaVBYcTZCSjFUdlJ2cEFjYSIsImlzX2FkbWluIjp0cnVlLCJpYXQiOjE1NzUwMTcwNTN9.eqtNHB03N5-cQRpWa72-MTXYO7AKJq5as5-_JwEyAp0')
 
@@ -78,7 +78,7 @@ describe('Red-flag/Intervention', () => {
     });
     describe('Post a red-flag/intervention', () => {
         it('User should receive an error create message about invalid location input not a number', (done) => {
-            supertest('http://localhost:8080/api/v1')
+            supertest('http://localhost:8080/api/v2')
                 .post('/red-flags')
                 .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RfbmFtZSI6ImJpaGlyZSIsImxhc3RfbmFtZSI6ImJvcmlzIiwiZW1haWwiOiJtdWhpcmVib3Jpc0B5YWhvby5mciIsInBob25lX251bWJlciI6IjEyMzQ1Njc4OTAiLCJwYXNzd29yZCI6IiQyYSQxMCROR0hPMWJzNHNhdXAzN0h3S3Mya3MuUVFyYkRMbHNpWDRHcFNOaVBYcTZCSjFUdlJ2cEFjYSIsImlzX2FkbWluIjp0cnVlLCJpYXQiOjE1NzUwMTcwNTN9.eqtNHB03N5-cQRpWa72-MTXYO7AKJq5as5-_JwEyAp0')
 
@@ -100,7 +100,7 @@ describe('Red-flag/Intervention', () => {
     });
     describe('Post a red-flag/intervention', () => {
         it('User should receive an error create message about no location', (done) => {
-            supertest('http://localhost:8080/api/v1')
+            supertest('http://localhost:8080/api/v2')
                 .post('/red-flags')
                 .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RfbmFtZSI6ImJpaGlyZSIsImxhc3RfbmFtZSI6ImJvcmlzIiwiZW1haWwiOiJtdWhpcmVib3Jpc0B5YWhvby5mciIsInBob25lX251bWJlciI6IjEyMzQ1Njc4OTAiLCJwYXNzd29yZCI6IiQyYSQxMCROR0hPMWJzNHNhdXAzN0h3S3Mya3MuUVFyYkRMbHNpWDRHcFNOaVBYcTZCSjFUdlJ2cEFjYSIsImlzX2FkbWluIjp0cnVlLCJpYXQiOjE1NzUwMTcwNTN9.eqtNHB03N5-cQRpWa72-MTXYO7AKJq5as5-_JwEyAp0')
 
@@ -121,28 +121,3 @@ describe('Red-flag/Intervention', () => {
         });
     });
 });
-// describe('Red-flag/Intervention', () => {
-//     describe('Post a red-flag/intervention', () => {
-//         it('User should receive a successful create message', (done) => {
-//             supertest('http://localhost:8080/api/v1/')
-//                 .post('/red-flags')
-//                 .set('Accept', 'application/json')
-//                 .set('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZmlyc3RfbmFtZSI6ImJpaGlyZSIsImxhc3RfbmFtZSI6ImJvcmlzIiwiZW1haWwiOiJtdWhpcmVib3Jpc0B5YWhvby5mciIsInBob25lX251bWJlciI6IjEyMzQ1Njc4OTAiLCJwYXNzd29yZCI6IiQyYSQxMCROR0hPMWJzNHNhdXAzN0h3S3Mya3MuUVFyYkRMbHNpWDRHcFNOaVBYcTZCSjFUdlJ2cEFjYSIsImlzX2FkbWluIjp0cnVlLCJpYXQiOjE1NzUwMTcwNTN9.eqtNHB03N5-cQRpWa72-MTXYO7AKJq5as5-_JwEyAp0')
-
-//                 .send({
-//                     "title": "this is the tittle",
-//                     "labels": "1",
-//                     "type": "intervention",
-//                     "comment": "this is the comment about the red-flag",
-//                     "location": "-90,3"
-//                 })
-//                 .expect('Content-Type', /json/)
-//                 .end((err, res) => {
-//                     res.should.have.status(201);
-//                     res.should.be.a('object');
-//                     done();
-//                 });
-//         });
-//     });
-   
-// })
